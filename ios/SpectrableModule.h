846e914fdb9567739cd753e4b02ae733b5d36af0
manoj <it.spectraho@gmail.com>
2022-05-26 15:35:27 +0530

    Swift header file imported into spectrablemodule

--- ios/SpectrableModule.h
+++ ios/SpectrableModule.h
@@ -1,7 +1,7 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
 #import <React/RCTLog.h>
-
+#import "SpectraBLE-Swift.h"
 
 @interface SpectrableModule : RCTEventEmitter <RCTBridgeModule, BleManagerDelegate>
 
